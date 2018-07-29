d4502c95593222955023c41232cf4aec504d8c30
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-29 05:09:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var string = "Hello"
 function shout(string) {
   return string.toUpperCase()
 }
@@ -6,5 +6,5 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log($string).toUpperCase()
+  console.log(string).toUpperCase()
 }

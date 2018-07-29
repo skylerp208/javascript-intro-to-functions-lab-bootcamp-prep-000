0df9484757934779ced494fa8021f9dc8cb83662
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-29 05:55:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
   if(string === 'hello') {
     return "I can't hear you!";
   } else if (string === 'HELLO') {
-      return "HELLO";
+      return "YES INDEED!";
   } else if (string === "I love you, Grandma." ) {
     return "I love you, too.";
   }}
